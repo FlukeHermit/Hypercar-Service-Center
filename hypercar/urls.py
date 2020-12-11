163b24124628847fba72e94b131a3b24f59212b5
FlukeHermit <zayed.humayun@g.bracu.ac.bd>
2020-12-12 02:14:01 +0600

    Operator menu

--- hypercar/urls.py
+++ hypercar/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, re_path
-from tickets.views import WelcomeView, MenuView, OperationView
+from tickets.views import WelcomeView, MenuView, OperationView, ProcessingView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('welcome/', WelcomeView.as_view()),
     path('menu/', MenuView.as_view()),
-    re_path('get_ticket/(?P<operation>\w+)', OperationView.as_view())
+    re_path('get_ticket/(?P<operation>\w+)', OperationView.as_view()),
+    path('processing', ProcessingView.as_view()),
+    path('processing/', RedirectView.as_view(url='/processing'))
 ]
 # \d+ means one or more digit [0-9] (depending on LOCALE)
 # \d- means a digit followed by a dash -
--- tickets/views.py
+++ tickets/views.py
@@ -4,19 +4,20 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 operations = {
-                 'change_oil': {
-                     'name': 'Change oil',
-                     'minutes_for_operation': 2,
-                 },
-                 'inflate_tires': {
-                     'name': 'Inflate tires',
-                     'minutes_for_operation': 5,
-                 },
-                 'diagnostic': {
-                     'name': 'Get diagnostic test',
-                     'minutes_for_operation': 30,
-                 }
-             }
+    'change_oil': {
+        'name': 'Change oil',
+        'minutes_for_operation': 2,
+    },
+    'inflate_tires': {
+        'name': 'Inflate tires',
+        'minutes_for_operation': 5,
+    },
+    'diagnostic': {
+        'name': 'Get diagnostic test',
+        'minutes_for_operation': 30,
+    }
+}
+
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
@@ -33,13 +34,17 @@ class OperationView(TemplateView):
     template_name = 'tickets/operation.html'
 
     queue = {}
+    length = {'change_oil': 0,
+              'inflate_tires': 0,
+              'diagnostic': 0}
 
-    def get_ticket_number(self):
+    def get_ticket_number(self, operation):
         count_of_elements = 0
 
         for x in self.queue.values():
             count_of_elements += x
 
+        self.length[operation] += 1
         return count_of_elements + 1
 
     def get_time_to_wait(self, operation):
@@ -48,7 +53,8 @@ class OperationView(TemplateView):
                  'diagnostic': 0}
 
         for x in times.keys():
-            times[x] = self.queue.get(x, 0) * operations[x]['minutes_for_operation']
+            times[x] = self.queue.get(
+                x, 0) * operations[x]['minutes_for_operation']
 
         times['inflate_tires'] += times['change_oil']
         times['diagnostic'] += times['inflate_tires']
@@ -63,9 +69,22 @@ class OperationView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['operation'] = operation
         context['title'] = operations[operation]['name']
-        context['ticket_number'] = self.get_ticket_number()
+        context['ticket_number'] = self.get_ticket_number(operation)
         context['minutes_to_wait'] = self.get_time_to_wait(operation)
 
         self.queue[operation] = self.queue.get(operation, 0) + 1
-        
+
+        return context
+
+
+class ProcessingView(TemplateView):
+
+    template_name = "tickets/processing.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['lines'] = {'Change oil': OperationView.length['change_oil'],
+                            'Inflate tires': OperationView.length['inflate_tires'],
+                            'Get diagnostic': OperationView.length['diagnostic']}
         return context
